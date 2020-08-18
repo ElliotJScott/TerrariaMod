50149f259e777bae70f4fc3dbe39203548a69d25
Elliot Scott <elliotscott99@hotmail.co.uk>
2020-08-18 17:10:07 +0100

    Weapon Sprites

---
+++ Items/Upgrades/Upgrade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace StarSailor.Items.Upgrades
+{
+    abstract class Upgrade : ModItem
+    {
+        public abstract Item InputItem { get; }
+        public abstract Item OutputItem { get; }
+        public override void RightClick(Player player)
+        {
+            if (player.inventory.Contains(InputItem))
+            {
+                int i = player.inventory.ToList().IndexOf(InputItem);
+                player.inventory[i] = OutputItem;
+            }
+            base.RightClick(player);
+        }
+    }
+}
